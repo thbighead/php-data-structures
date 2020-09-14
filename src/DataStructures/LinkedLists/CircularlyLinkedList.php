ea05a62aaccf5b733068464145114f1ab576c9ff
Thales Nathan <thcmatias@gmail.com>
2020-09-14 19:53:04 -0300

    First circular linked list implementation
    pending tests.

---
+++ src/DataStructures/LinkedLists/CircularlyLinkedList.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace TNCPHP\DataStructures\LinkedLists;
+
+use TNCPHP\DataStructures\LinkedLists\Strategies\Circular;
+
+/**
+ * Class SinglyLinkedList
+ */
+class CircularlyLinkedList extends SinglyLinkedList
+{
+    use Circular;
+}
